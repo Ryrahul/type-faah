7cd558249fb38b61aac64fecd2d87b078fe96699
ryrahul <caulirahul6@gmail.com>
2026-03-14 16:01:03 +0545

    style: capitalize B in browser title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "bakchodi / type",
+  title: "Bakchodi / type",
   description: "An aesthetic keyboard typing test",
   icons: {
     icon: "/favicon.png",
